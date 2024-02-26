d4da3de5f1c77c321e1a98686c75e1bd26422986
yy11038158 <134906283+yy11038158@users.noreply.github.com>
2024-02-26 23:48:07 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "恒熠科技AI",
+  description: "让ChatGPT成为你的工作助手！",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "恒熠科技AI",
     statusBarStyle: "default",
   },
 };
